3108883cf94b05ab2285e69a131795ff4792c68f
hexi0721 <M4N4HQHQ@gmail.com>
2024-12-03 21:58:48 +0800

    12_3-1
    card 改變動畫速度

--- Assets/Scripts/Present2/OpenCard.cs
+++ Assets/Scripts/Present2/OpenCard.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenCard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject scratchZone , card;
+
+    bool isFadeIn;
+    
+
+    private void Awake()
     {
+        card.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(OpenCardFunc);
+
+    }
+
+    private void Start()
+    {
+        scratchZone.SetActive(false);
+        isFadeIn = false;
+    }
+
+
+    private void Update()
+    {
+        if (isFadeIn)
+        {
+            FadeIn();
+        }
+    }
+
+    public void OpenCardFunc()
+    {
+        card.transform.GetChild(0).gameObject.SetActive(false); // touchButton
+        card.transform.GetChild(1).gameObject.SetActive(false); // openText
+
+        GetComponent<Animator>().enabled = true;
+
+        // StartCoroutine(ExampleCoroutine());
+        
+        
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+    IEnumerator ExampleCoroutine()
     {
-        
+
+        yield return new WaitForSeconds(3.0f);
+
+
+        card.SetActive(false);
+        scratchZone.SetActive(true);
+
+        isFadeIn = true;
     }
+    */
+
+    public void AnimatorEnd()
+    {
+        card.SetActive(false);
+        scratchZone.SetActive(true);
+
+        isFadeIn = true;
+    }
+
+    
+    void FadeIn()
+    {
+
+        if(scratchZone.GetComponent<CanvasGroup>().alpha == 1)
+        {
+            return;
+        }
+
+        scratchZone.GetComponent<CanvasGroup>().alpha += Time.deltaTime;
+    }
+
 }
