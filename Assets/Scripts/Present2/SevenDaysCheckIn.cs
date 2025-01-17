334c22fb4740caedf0ff8b3ccf63a5dc07448663
hexi0721 <M4N4HQHQ@gmail.com>
2025-01-17 21:56:04 +0800

    1_17-1

--- Assets/Scripts/Present2/SevenDaysCheckIn.cs
+++ Assets/Scripts/Present2/SevenDaysCheckIn.cs
@@ -7,6 +7,10 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Globalization;
+using TMPro;
 
 public class SevenDaysCheckIn : MonoBehaviour
 {
@@ -30,10 +34,49 @@ public class SevenDaysCheckIn : MonoBehaviour
 
     }
 
+    public class TimeFetcher 
+    {
+        public DateTime GetNetworkTime()
+        {
+            const string url = "https://www.google.com";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "HEAD";
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    string dateHeader = response.Headers["date"];
+                    if (DateTime.TryParseExact(
+                        dateHeader,
+                        "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.AdjustToUniversal,
+                        out DateTime networkDateTime))
+                    {
+                        return networkDateTime.ToLocalTime();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"獲取網路時間失敗：{e.Message}");
+                
+            }
+
+            return new DateTime(); // 如果失敗，返回一個默認值
+        }
+    }
+
+
+
     DateTime birthDay;
-    [SerializeField] int startDay , span ;
+    [SerializeField] int span ;
+
 
     [SerializeField] GameObject checkInBoxContainer;
+    [SerializeField] GameObject hintContainer;
     [SerializeField] List<CheckInBox> checkInBoxList;
     public Sprite sprite;
     public int loginDay;
@@ -42,6 +85,12 @@ public class SevenDaysCheckIn : MonoBehaviour
 
     [SerializeField] GameObject continueButton;
     [SerializeField] GameObject birthdayCard;
+    [SerializeField] TextMeshProUGUI checkInText;
+    Color currentColor = Color.red; // 初始為紅色
+    [SerializeField] int colorState = 0;
+    [SerializeField] Text timeIncorrectWarningText;
+
+    TimeFetcher timeFetcher = new TimeFetcher(); // class TimeFetcher 
 
 
     private void Start()
@@ -57,23 +106,30 @@ public class SevenDaysCheckIn : MonoBehaviour
             { 6 , "Day7"}
         };
 
-        
-
         birthdayCard.SetActive(false);
         continueButton.SetActive(false);
 
-        // 修改生日時間
-        birthDay = new DateTime(DateTime.Now.Year, 2, 17, 0, 0, 0);
+        birthDay = new DateTime(DateTime.Now.Year, 1, 17, 0, 0, 0); // 修改生日時間
 
-        DateTime _today = DateTime.Today;
+        //DateTime _today = DateTime.Today;
+        DateTime _today = timeFetcher.GetNetworkTime();
+
+        if(_today.Year == 1) // 獲取網路時間失敗
+        {
+            checkInBoxContainer.SetActive(false);
+            hintContainer.SetActive(false);
+            timeIncorrectWarningText.text = "開啟網路，獲取正確時間，難不成你想偷作弊?";
+            //Debug.Log($"網路時間是：{_today}");
+            return;
+        }
+        
         if (birthDay <= new DateTime(DateTime.Now.Year, 1, 6, 0, 0, 0) && birthDay >= new DateTime(DateTime.Now.Year, 1, 1, 0, 0, 0) && _today.Month == 12) // 生日是1/6以前 1/1之後 而登入時間在12月
         {
             birthDay = birthDay.AddYears(1);
         }
-        //
-
+        
         DateTime startDay = birthDay.AddDays(-6);
-        if (startDay <= _today && _today <= birthDay)
+        if (startDay <= _today && _today.Day <= birthDay.Day)
         {
             span = Mathf.Abs((_today - startDay).Days);
             string tmpString = "Day" + (span + 1).ToString();
@@ -111,25 +167,18 @@ public class SevenDaysCheckIn : MonoBehaviour
         {
             DeletePlayerPrefsAll();
         }
-        
-        
-        
-        
-
 
     }
 
     private void Update()
     {
-        
-
+        TitleRGB();
         OntheBirthDay(); // 在生日當天，開啟繼續按鈕
 
         // 測試用
-        /*
-        DeletePlayerPrefs();
-        DeletePlayerPrefsAll();
-        */
+        // DeletePlayerPrefs();
+        // DeletePlayerPrefsAll();
+
     }
 
     private void DeletePlayerPrefsAll()
@@ -261,4 +310,82 @@ public class SevenDaysCheckIn : MonoBehaviour
         }
 
     }
+
+    void TitleRGB()
+    {
+        float delta = Time.deltaTime ;
+
+        switch (colorState)
+        {
+            case 0: // R 增加
+                
+                currentColor.r += delta;
+                if (currentColor.r >= 1f)
+                {
+                    currentColor.r = 1f;
+                    
+                    colorState = 1; 
+                }
+                break;
+
+            case 1: // G 減少
+                
+                currentColor.g -= delta;
+                if (currentColor.g <= 0.5f)
+                {
+                    
+                    currentColor.g = 0.5f;
+                    colorState = 2; 
+                }
+                break;
+
+            case 2: // B 增加
+
+                currentColor.b += delta;
+                if (currentColor.b >= 1f)
+                {
+
+                    currentColor.b = 1f;
+                    colorState = 3;
+                }
+                break;
+
+            case 3: // R 減少
+
+                currentColor.r -= delta;
+                if (currentColor.r <= 0.5f)
+                {
+                    currentColor.r = 0.5f;
+
+                    colorState = 4;
+                }
+                break;
+
+            case 4: // G 增加
+
+                currentColor.g += delta;
+                if (currentColor.g >= 1f)
+                {
+
+                    currentColor.g = 1f;
+                    colorState = 5;
+                }
+                break;
+
+            case 5: // B 減少
+
+                currentColor.b -= delta;
+                if (currentColor.b <= 0.5f)
+                {
+
+                    currentColor.b = 0.5f;
+                    colorState = 0;
+                }
+                break;
+
+        }
+
+        checkInText.color = currentColor;
+    }
+
 }
